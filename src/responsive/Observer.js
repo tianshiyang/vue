cfbe6303424d7e626ccc8d0da8629597c679ad29
unknown <1685821150@qq.com>
2021-12-01 23:16:45 +0800

    feat: 注释

--- src/responsive/Observer.js
+++ src/responsive/Observer.js
@@ -3,10 +3,12 @@ class Observer {
   constructor(data) {
     this.data = data;
     this.observer(data);
+    // Dep为订阅者，关于Dep的使用，在下文详解
     this.dep = new Dep();
   }
   observer(data) {
     if (!data || typeof data !== "object") {
+      // data不存在，或者data为基础数据类型
       return;
     }
     Object.keys(data).forEach((key) => {
@@ -21,8 +23,9 @@ class Observer {
       enumerable: true,
       configurable: true,
       get() {
-        // Dep.target当前Watcher的实例
+        // Dep.target为当前Watcher的实例，它具有update，和get方法
         if (Dep.target) {
+          // 将当前实例化的watcher，保存到Dep订阅者数组中
           self.dep.addSub(Dep.target);
         }
         return val;
@@ -32,6 +35,7 @@ class Observer {
           return;
         }
         val = newVal;
+        // 下文详解Dep作用，现在不用关心，到此知道这个属性被监听到了即可
         self.dep.notify();
       },
     });
